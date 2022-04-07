bfe0a8e22dc82d321ffef4823acd82c3102599e6
VadimArkhipov <arkhipov_vadim@inbox.ru>
2022-04-07 15:07:59 +0300

    lab4

---
+++ KMP.h
@@ -0,0 +1,10 @@
+#ifndef UNTITLED_KMP_H
+#define UNTITLED_KMP_H
+
+#include <vector>
+#include <string>
+#include "prefix_func.h"
+
+std::vector<int> kmp (std::string pattern, std::string text);
+
+#endif //UNTITLED_KMP_H
---
+++ prefix_func.h
@@ -0,0 +1,12 @@
+#ifndef UNTITLED_PREFIX_FUNC_H
+#define UNTITLED_PREFIX_FUNC_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+extern bool DEBUG;
+std::vector<int> prefix_func(std::string str);
+void print_tab(int t);
+void print_space(int s);
+#endif //UNTITLED_PREFIX_FUNC_H
